08fa05a8e626c396350ca568f6817872b1fed0bf
saratkavuru <kavurusarat@gmail.com>
2016-09-20 19:26:50 -0400

    model validations.

--- library_app/app/models/room.rb
+++ library_app/app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
   belongs_to :library
+  validates :room_number, :presence => true
+  validates_numericality_of :capacity, :message=>"Room size should be numerical"
+  validates :library_id, :presence => true
+
+
 end
