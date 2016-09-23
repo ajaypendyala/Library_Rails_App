289c17867b1e851083168520f6920d5a9ca933f3
Tanmay Goel <goeltanmay@gmail.com>
2016-09-23 19:21:22 -0400

    room list now shows status and book now option. clicking on room id takes you to the room page, where you can see the room's booking history.

--- library_app/app/controllers/rooms_controller.rb
+++ library_app/app/controllers/rooms_controller.rb
@@ -1,5 +1,10 @@
 class RoomsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+  def index
     @rooms = Room.all
+  end
+
+  def show
+  	@room = Room.find(params[:id])
   end
 end
