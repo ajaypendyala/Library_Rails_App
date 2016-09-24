372488548f51f0cc1b1b791be2dc0d0e38d68cee
saratkavuru <kavurusarat@gmail.com>
2016-09-24 17:58:10 -0400

    GIT asked me to

--- library_app/config/routes.rb
+++ library_app/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :bookings
+  get "book_new" => "bookings#new", as: "book_new"
   resources :rooms
 
   get "booking_history" => "users#booking_history", as: "booking_history"
