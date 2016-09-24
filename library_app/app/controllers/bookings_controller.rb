7ebf8af3e488b404b82682e67b71fe3cd4955fb4
Ajay Chandra Pendyala <ajaypendyala@Ajays-MacBook-Pro.local>
2016-09-24 15:55:44 -0400

    bookingsformrooms

--- library_app/app/controllers/bookings_controller.rb
+++ library_app/app/controllers/bookings_controller.rb
@@ -4,4 +4,21 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     # @booking.user = current_user
   end
+
+  def show
+  	@booking = Booking.all
+  end
+
+  def create
+  	@booking = Booking.new(booking_params)
+
+        	@booking.save!
+        flash[:notice]='Booking created. Admin will review the request.'
+        redirect_to ("/rooms/")
+  end
+
+
+  def booking_params
+      		params.require(:booking).permit(:Room,:start_time)
+  end
 end
--- library_app/app/models/room.rb
+++ library_app/app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :library
+  has_many :booking
   # has_one :library
   # attr_accessible :room_number, :library_id
 
