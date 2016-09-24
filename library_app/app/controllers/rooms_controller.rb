dd65fa00a31a89152ba2cdbd455912ef55a1606e
Tanmay Goel <goeltanmay@gmail.com>
2016-09-24 04:22:04 -0400

    some part of search working

--- library_app/app/controllers/rooms_controller.rb
+++ library_app/app/controllers/rooms_controller.rb
@@ -1,10 +1,41 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @rooms = Room.all
+  	# params.each {|param, val| puts "#{param} : #{val}"}
+  	#search  - room number .. .
+  	# if params.has_key?(:search)
+  	# 	params = search_params_cleaner(params)	
+  	# end
+  	search={}
+  	@libraries = Library.all  #for the dropdown
+  	if(params.has_key? "search")
+  		params[:search].each do |key, val|
+	  		unless val == ""
+	  			search[key] = val
+	  		end
+  		end
+  		puts search.to_s
+  		@rooms = Room.where search
+  	else
+    	@rooms = Room.all
+    end
   end
 
   def show
   	@room = Room.find(params[:id])
+  end
+
+  def search_params_cleaner(params)
+  	params[:search].each do |key, val|
+  		if val == ""
+  			params[:search].delete(key)
+  		end
+  	end
+  	return params
+  end
+
+  def create_datetime_from_params
+  	if params[:search][:date] and params[:search][:time]
+  	end
   end
 end
