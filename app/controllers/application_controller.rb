9efafdbab72b68c6264283520e95f970cf65bfbc
saratkavuru <kavurusarat@gmail.com>
2016-09-25 19:33:15 -0400

    Rescue in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
    redirect_to "/users/sign_in/", :notice => exception.message
   end
-  #rescue_from Exception do |exception|
-  #	redirect_to "/rooms", :notice => exception.message
-  #end
+  rescue_from Exception do |exception|
+  	redirect_to "/rooms", :notice => exception.message
+  end
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
