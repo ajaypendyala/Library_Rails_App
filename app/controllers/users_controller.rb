af174046bcd18a0b2ceedc7e24986b8296a11463
Tanmay Goel <goeltanmay@gmail.com>
2016-09-25 14:32:50 -0400

    navbar with relevant links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 	def booking_history
-		if current_user
-	  		if current_user.is_admin? or current_user.is_super_admin?
-	  			redirect_to '/admin'
-	  		end
-  		end
 	end
 end
