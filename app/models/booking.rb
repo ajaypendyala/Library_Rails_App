17e2193e8020cc12d0ad8aadb0b8a92c4451cb87
Tanmay Goel <goeltanmay@gmail.com>
2016-09-25 15:20:36 -0400

    check if room is available before booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,15 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  before_save :check_if_booking_possible?
   # attr_accessible :room_id, :user_id, :start_time
   validates :user, :presence => true
   validates :room, :presence => true
   validates :start_time, :presence => true
+  protected
+  def check_if_booking_possible?
+  	unless self.room.available?(self.start_time)
+  		raise 'Room Not Available'
+  	end
+  end
 end
