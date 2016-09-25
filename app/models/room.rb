6e91537b5dc6e63856d107d60d98c2bb2ebcf154
saratkavuru <kavurusarat@gmail.com>
2016-09-25 16:42:05 -0400

    Enum for capacity

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,7 +7,7 @@ class Room < ApplicationRecord
   validates :room_number, :uniqueness => {:scope => :library}
   validates :capacity, :presence => true
   validates :library, :presence => true
-
+  enum capacity: { Small: 4, Medium: 6, Large:12 }
 
   def to_s
     return self.library.name + ", Room " + self.room_number.to_s
