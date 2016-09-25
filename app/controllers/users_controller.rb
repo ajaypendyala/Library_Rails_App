d37d8ea094554e23f54a8691f97301bc0deba98d
Tanmay Goel <goeltanmay@gmail.com>
2016-09-24 21:12:05 -0400

    bugfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 	def booking_history
+		if current_user
+	  		if current_user.is_admin? or current_user.is_super_admin?
+	  			redirect_to '/admin'
+	  		end
+  		end
 	end
 end
