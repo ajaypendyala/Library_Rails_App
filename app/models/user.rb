71562c6192507de2fd4e1abe20a140d4e3418b58
saratkavuru <kavurusarat@gmail.com>
2016-09-25 15:05:55 -0400

    Cascade delete for corner cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   rolify
-  has_many :Booking
+  has_many :Booking , :dependent => :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
