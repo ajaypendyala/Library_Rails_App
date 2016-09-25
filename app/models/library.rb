60f63c1fcc77bd6b5e6645bf145d05995d56b00c
saratkavuru <kavurusarat@gmail.com>
2016-09-25 16:32:14 -0400

    Enum lib

--- app/models/library.rb
+++ app/models/library.rb
@@ -2,7 +2,7 @@ class Library < ApplicationRecord
   # attr_accessible :name
   has_many :room
   validates :name , :presence => true
-
+  enum name: [ :JamesHunt, :DHHill ]
   def to_s
     return self.name
   end
