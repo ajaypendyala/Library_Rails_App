338b6f833d498179216c0fa3ca7234383cc90de7
saratkavuru <kavurusarat@gmail.com>
2016-09-25 19:19:03 -0400

    Added username to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from CanCan::AccessDenied do |exception| 
-    redirect_to "/users/sign_in/", :notice => exception.message
+  rescue_from CanCan::AccessDenied do |exception|
+   redirect_to "/users/sign_in/", :notice => exception.message
   end
-  rescue_from Exception do |exception|
-  	redirect_to "/rooms", :notice => exception.message
+  #rescue_from Exception do |exception|
+  #	redirect_to "/rooms", :notice => exception.message
+  #end
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password])
   end
 end
