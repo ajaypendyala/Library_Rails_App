ecf4213ec901b6f9b60833f68addbb2ed90f23ec
Tanmay Goel <goeltanmay@gmail.com>
2016-09-25 19:49:16 -0400

    delete future bookings for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception| 
     redirect_to "/users/sign_in/", :notice => exception.message
   end
-  rescue_from Exception do |exception|
-  	redirect_to "/rooms", :notice => exception.message
-  end
+  # rescue_from Exception do |exception|
+  # 	redirect_to "/rooms", :notice => exception.message
+  # end
 end
