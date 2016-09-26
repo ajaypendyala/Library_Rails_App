9f4c5711c020e23c45656b520107f10fad57c9fe
saratkavuru <kavurusarat@gmail.com>
2016-09-25 20:51:13 -0400

    EXtra emails

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get "booking_history" => "users#booking_history", as: "booking_history"
 
+  get "invite" => "bookings#invite", as: "invite"
   root 'users#booking_history'
 
 end
