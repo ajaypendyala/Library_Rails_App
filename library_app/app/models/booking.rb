cd2119b818f6ac72ba9d4a716b1fd3d0487ae238
Tanmay Goel <goeltanmay@gmail.com>
2016-09-25 20:10:49 -0400

    trying to delete unnecessary files.

--- library_app/app/models/booking.rb
+++
@@ -1,8 +0,0 @@
-class Booking < ApplicationRecord
-  belongs_to :User
-  belongs_to :Room
-  # attr_accessible :room_id, :user_id, :start_time
-  validates :User, :presence => true
-  validates :Room, :presence => true
-  validates :start_time, :presence => true
-end
